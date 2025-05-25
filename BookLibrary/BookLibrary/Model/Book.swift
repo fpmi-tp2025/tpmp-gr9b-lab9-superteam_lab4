a2e42db14eac70228e49f32d1b87c3a156147045
AlekseiNarkevich <alesia.narkevich82@gmail.com>
2025-05-25 17:20:19 +0300

    Очистка репозитория

--- BookLibrary/BookLibrary/Model/Book.swift
+++
@@ -1,7 +0,0 @@
-/// Модель данных книги
-struct Book {
-    let title: String      // Название книги
-    let author: String     // Автор книги
-    let description: String // Описание книги
-    let imageName: String  // Название изображения книги
-}
